edd9e513a4bd7025fb9bc2b7bfc762a2d5291518
Steven Hayes <asiansteev@gmail.com>
2014-02-25 18:46:12 -0500

    search for that lobster son

--- search.rb
+++ search.rb
@@ -1,2 +1,10 @@
 require 'twitter'
+require 'yaml'
 
+credentials = YAML.load_file('credentials.yml')
+client = Twitter::REST::Client.new(credentials)
+
+client.search("lobster for dinner", result_type: "recent").
+       take(3).each do |tweet|
+    puts tweet.text
+end
