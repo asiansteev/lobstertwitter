95722ff5c069dbef763431dd0bc2ec71e2a4eb19
Steven Hayes <asiansteev@gmail.com>
2014-02-25 19:44:21 -0500

    there it is!

--- search.rb
+++ search.rb
@@ -4,7 +4,10 @@ require 'yaml'
 credentials = YAML.load_file('credentials.yml')
 client = Twitter::REST::Client.new(credentials)
 
-client.search("lobster for dinner", result_type: "recent").
-       take(3).each do |tweet|
-    puts tweet.text
+client.search('lobster for dinner', result_type: 'recent').
+       take(1).each do |tweet|
+    # without the @username in the tweet, the conversation doesn't appear
+    # on twitter
+    client.update "@#{tweet.user.screen_name} mmm... that sounds delicious",
+                  in_reply_to_status_id: tweet.id.to_s
 end
