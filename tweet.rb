a7f224188a3882f22b2a7d0bb33d629f7215d939
Steven Hayes <asiansteev@gmail.com>
2014-02-26 01:26:03 -0500

    say what you mean

--- tweet.rb
+++ tweet.rb
@@ -7,9 +7,9 @@ credentials = YAML::load(File.open(fn))
 client = Twitter::REST::Client.new(credentials)
 
 # have we already replied to this user?
-def already_replied? client, tweet
+def already_replied? client, user
   client.user_timeline("crustasianwill").any?{|my_tweet|
-    my_tweet.in_reply_to_user_id == tweet.user.id
+    my_tweet.in_reply_to_user_id == user.id
   }
 end
 
@@ -21,7 +21,7 @@ end
 # -rt excludes retweets
 tweet = client.search('lobster for dinner -rt', result_type: 'recent').first
 
-unless already_replied?(client, tweet)
+unless already_replied?(client, tweet.user)
   # without @username in the tweet, the conversation doesn't appear on twitter
   message = "@#{tweet.user.screen_name} #{random_phrase}"
   client.update message, in_reply_to_status_id: tweet.id.to_s
