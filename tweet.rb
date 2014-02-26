88b8ba2b7e8f869c8cbdbe2a215b33bd4e95183e
Steven Hayes <asiansteev@gmail.com>
2014-02-26 00:02:05 -0500

    only search for nonretweets and don't reply...
    if already replied

--- tweet.rb
+++ tweet.rb
@@ -4,10 +4,28 @@ require 'yaml'
 credentials = YAML.load_file('credentials.yml')
 client = Twitter::REST::Client.new(credentials)
 
-client.search('lobster for dinner', result_type: 'recent').
+def already_replied? client, tweet
+  client.user_timeline("crustasianwill").any?{|my_tweet|
+    my_tweet.id == tweet.id
+  }
+end
+
+def random_phrase
+  ['mmm... that sounds delicious!',
+   'bring me some!',
+   'i\'m salivating...',
+   'hook it up!',
+   'that\'s my favorite.'
+  ].sample
+end
+
+# -rt excludes retweets
+client.search('lobster for dinner -rt', result_type: 'recent').
        take(1).each do |tweet|
+  unless tweet.retweeted == 'true' || already_replied?(client, tweet)
     # without the @username in the tweet, the conversation doesn't appear
     # on twitter
-    client.update "@#{tweet.user.screen_name} mmm... that sounds delicious",
+    client.update "@#{tweet.user.screen_name} #{random_phrase}",
                   in_reply_to_status_id: tweet.id.to_s
+  end
 end
