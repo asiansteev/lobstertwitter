eb5b4458f0ec49f6580b288fe0da423ff7a54654
Steven Hayes <asiansteev@gmail.com>
2014-02-26 00:10:24 -0500

    don't reply to the same _user_ twice

--- tweet.rb
+++ tweet.rb
@@ -4,9 +4,10 @@ require 'yaml'
 credentials = YAML.load_file('credentials.yml')
 client = Twitter::REST::Client.new(credentials)
 
+# have we already replied to this user?
 def already_replied? client, tweet
   client.user_timeline("crustasianwill").any?{|my_tweet|
-    my_tweet.id == tweet.id
+    my_tweet.in_reply_to_user_id == tweet.user.id
   }
 end
 
@@ -20,12 +21,12 @@ def random_phrase
 end
 
 # -rt excludes retweets
-client.search('lobster for dinner -rt', result_type: 'recent').
-       take(1).each do |tweet|
-  unless tweet.retweeted == 'true' || already_replied?(client, tweet)
-    # without the @username in the tweet, the conversation doesn't appear
-    # on twitter
-    client.update "@#{tweet.user.screen_name} #{random_phrase}",
-                  in_reply_to_status_id: tweet.id.to_s
-  end
+tweet = client.search('lobster for dinner -rt', result_type: 'recent').
+               take(1).first
+
+unless tweet.retweeted == 'true' || already_replied?(client, tweet)
+  # without the @username in the tweet, the conversation doesn't appear
+  # on twitter
+  client.update "@#{tweet.user.screen_name} #{random_phrase}",
+                in_reply_to_status_id: tweet.id.to_s
 end
